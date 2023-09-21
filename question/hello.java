bebf381bb67e000dbeb7aa69c3be566a4beae2bd
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-09-21 23:30:30 +0530

    resume

---
+++ question/hello.java
@@ -0,0 +1,28 @@
+class hello{
+    public static void main(String[] s) {
+        System.out.println(s[0]);
+        System.out.println(s[1]);
+        System.out.println(s[2]);
+        System.out.println("this is string");
+        System.out.println("this is second line");
+
+        
+    }
+     public static void main(int[] s) {
+        System.out.println(s[0]);
+        System.out.println(s[1]);
+        System.out.println(s[2]);
+        System.out.println("this is integer");
+
+        
+    }
+     public static void main(char[] s) {
+        System.out.println(s[0]);
+        System.out.println(s[1]);
+        System.out.println(s[2]);
+        System.out.println("this is character");
+
+        
+    }
+    
+}

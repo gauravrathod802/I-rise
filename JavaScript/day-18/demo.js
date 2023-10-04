2482f78af930a1efa6990721e01f03df35e28fb5
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-10-04 22:23:54 +0530

    javascript

---
+++ JavaScript/day-18/demo.js
@@ -0,0 +1,7 @@
+var a=30,b=30;
+
+console.log("Addition: ",a+b);
+console.log("Substraction: ",a-b);
+console.log("Multiplication: ",a*b);
+console.log("Division: ",a/b);
+console.log("Modulus: ",a%b);
